b60dd794f8aa448535faf70883b9e5af536fd0f3
AndrewAbe <raemus99@gmail.com>
2024-04-13 15:29:48 -0700

    GAN

--- dense.py
+++ dense.py
@@ -78,7 +78,7 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
 # Training
-def train(model, train_loader, loss_fn, optimizer, num_epochs=10):
+def train(model, train_loader, loss_fn, optimizer, num_epochs=0): #######################CHANGE
     for epoch in range(num_epochs):
         model.train()
         for batch, (X, y) in enumerate(train_loader):
@@ -115,3 +115,193 @@ train(model, train_loader, loss_fn, optimizer)
 # Evaluating the model
 accuracy = evaluate(model, test_loader)
 print(f'Test accuracy: {accuracy}')
+
+
+
+
+
+#CONVOLUTIONAL
+import torch.nn.functional as F
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        # Assuming input images are 28x28 pixels
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)  # 1 input channel, 32 output channels
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # Pooling layer
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)  # After two pooling operations, 28x28 becomes 7x7
+        self.fc2 = nn.Linear(128, 26)  # Assuming 26 output classes for letters
+
+    def forward(self, x):
+        # Reshape the input to have the channels dimension
+        x = x.view(-1, 1, 28, 28)  # Assuming x is of shape [batch_size, 784]
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)  # Flatten the tensor for the fully connected layer
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# Create the network
+model = ConvNet()
+
+# Loss and Optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+def train(model, train_loader, loss_fn, optimizer, num_epochs=0): #############################CHANGE
+    model.train()  # Set the model to training mode
+    for epoch in range(num_epochs):
+        total_loss = 0
+        for batch, (X, y) in enumerate(train_loader):
+            # Forward pass: compute the predicted outputs by passing inputs to the model
+            pred = model(X)
+            loss = loss_fn(pred, y)
+
+            # Backward pass: compute gradient of the loss with respect to model parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        #print(f'Epoch {epoch}: Loss: {total_loss/len(train_loader)}')
+
+        # Evaluate after each epoch
+        train_accuracy = evaluate(model, train_loader)
+        validate_accuracy = evaluate(model, validate_loader)
+        print(f'Epoch {epoch}: Train Accuracy: {train_accuracy:.4f}, Validate Accuracy: {validate_accuracy:.4f}')
+
+def evaluate(model, loader):
+    model.eval()  # Set the model to evaluation mode
+    correct = 0
+    total = 0
+    with torch.no_grad():  # No gradients needed
+        for X, y in loader:
+            pred = model(X)
+            predicted = torch.argmax(pred, axis=1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+
+    accuracy = correct / total
+    return accuracy
+
+# Assuming you have the DataLoader and CNN model set up
+model = ConvNet()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+# Train the model
+train(model, train_loader, loss_fn, optimizer)
+
+# Evaluate the model
+test_accuracy = evaluate(model, test_loader)
+print(f'Test accuracy: {test_accuracy:.4f}')
+
+
+
+
+#Generative adversarial network
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            # Input: Latent vector z (size of 100)
+            nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # Size: (512, 4, 4)
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # Size: (256, 8, 8)
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # Size: (128, 16, 16)
+            nn.ConvTranspose2d(128, 1, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+            # Output Size: (1, 28, 28)
+        )
+
+    def forward(self, z):
+        return self.model(z)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            # Input: Image (1, 28, 28)
+            nn.Conv2d(1, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # Size: (128, 14, 14)
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # Size: (256, 7, 7)
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # Size: (512, 4, 4)
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+            # Output: a single scalar
+        )
+
+    def forward(self, img):
+        return self.model(img)
+
+# Create instances of the Generator and Discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+# Loss function
+criterion = nn.BCELoss()
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+import torch
+
+# Number of epochs to train for
+num_epochs = 50
+
+# Training loop
+for epoch in range(num_epochs):
+    for i, (images, _) in enumerate(dataloader):  # Assuming images are the input data
+        # Training Discriminator
+        # Generate batch of latent vectors
+        noise = torch.randn(images.size(0), 100, 1, 1, device=device)
+
+        # Generate fake image batch with G
+        fake_images = generator(noise)
+
+        # Classify all fake batch with D
+        discriminator_real = discriminator(images).view(-1)
+        loss_D_real = criterion(discriminator_real, torch.ones_like(discriminator_real))
+
+        discriminator_fake = discriminator(fake_images.detach()).view(-1)
+        loss_D_fake = criterion(discriminator_fake, torch.zeros_like(discriminator_fake))
+
+        # Accumulate discriminator loss and backpropagate
+        loss_D = (loss_D_real + loss_D_fake) / 2
+        optimizer_D.zero_grad()
+        loss_D.backward()
+        optimizer_D.step()
+
+        # Training Generator
+        # Classify all fake batch with D
+        output = discriminator(fake_images).view(-1)
+        loss_G = criterion(output, torch.ones_like(output))
+
+        # Backpropagate generator loss
+        optimizer_G.zero_grad()
+        loss_G.backward()
+        optimizer_G.step()
+
+    # Output training stats
+    if (i+1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(dataloader)}], Loss_D: {loss_D.item()}, Loss_G: {loss_G.item()}')
